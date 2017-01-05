361b2789b7d72881fc0d9c256c9b06b77af389c3
predragcukalovic <predrag.cukalovic@gmail.com>
2017-01-05 20:53:04 +0100

    login pocetak

---
+++ PlanArtMVC/Controllers/LogInController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PlanArt;
+
+namespace PlanArtMVC.Controllers
+{
+    public class LogInController : Controller
+    {
+        //
+        // GET: /LogIn/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
