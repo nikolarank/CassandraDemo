46afcb33ebed4e0d8608b0fc21972927010472c1
predragcukalovic <predrag.cukalovic@gmail.com>
2017-01-05 01:05:10 +0100

    QueryEntities

---
+++ PlanArt/QueryEntities/Festival.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanArt.QueryEntities
+{
+    public class Festival
+    {
+        public Dictionary<string, string> artists { get; set; } //
+        public Dictionary<DateTime, Performance[]> calendar { get; set; } //
+        public string city { get; set; } //
+        public string email { get; set; } //
+        public Dictionary<string, string> festivals { get; set; } //
+        public string name { get; set; } //       
+    }
+}
---
+++ PlanArt/QueryEntities/Performance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanArt.QueryEntities
+{
+    public class Performance
+    {
+        public string performanceID { get; set; }
+    }
+}
