6e299ec941a7221f092d0f4982660ca42b7441e2
nikolarank <nikolarank94@gmail.com>
2017-01-15 23:28:03 +0100

    Redis pocetak

---
+++ PlanArtRedisCache/Data Layer/PostsCache.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlanArt.Data_Access;
+using ServiceStack.Redis;
+using ServiceStack.Text;
+using RedisDataLayer;
+
+namespace PlanArtRedisCache.Data_Layer
+{
+    public class PostsCache
+    {
+        List<Post> posts;
+        readonly RedisClient redis = new RedisClient(Config.SingleHost);
+
+        public void SaveToRedis(string email)
+        {
+            posts = Posts.Get(email);
+            var redisPosts = redis.As<Posts>();
+            redisPosts.Save();
+        }
+    }
+}
